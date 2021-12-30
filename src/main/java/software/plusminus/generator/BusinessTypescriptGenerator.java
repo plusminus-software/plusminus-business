9635c581adaa209f826921cab349fd8af0b41f01
Taras Shpek <taras.shpek@gmail.com>
2021-12-30 18:01:08 +0200

    fix BusinessTypescriptGenerator

--- src/main/java/software/plusminus/generator/BusinessTypescriptGenerator.java
+++ src/main/java/software/plusminus/generator/BusinessTypescriptGenerator.java
@@ -1,6 +1,7 @@
 package software.plusminus.generator;
 
 import org.springframework.stereotype.Component;
+import software.plusminus.business.model.BusinessEntity;
 import software.plusminus.generator.typescript.TypescriptGenerator;
 import software.plusminus.generator.typescript.model.TypescriptClass;
 
@@ -12,14 +13,18 @@ public class BusinessTypescriptGenerator extends TypescriptGenerator {
     @Override
     public TypescriptClass generateModel(Class<?> sourceClass) {
         TypescriptClass model = super.generateModel(sourceClass);
-        if (model.imports() == null) {
-            model.imports(new ArrayList<>());
+        if (sourceClass == BusinessEntity.class) {
+            if (model.imports() == null) {
+                model.imports(new ArrayList<>());
+            }
+            
+            model.imports().add("import { UUID } from 'angular2-uuid';");
+            model.imports().remove("import { UUID } from 'angular2-uuid';");
+
+            model.constructor("  super();\n"
+                    + "    this.uuid = UUID.UUID();\n"
+                    + "    this.modifiedTime = new Date();");
         }
-        model.imports().add("import { UUID } from 'angular2-uuid';");
-        
-        model.constructor("  super();\n"
-                + "  this.modifiedTime = new Date();\n"
-                + "  this.uuid = UUID.UUID();\n");
         return model;
     }
 }
